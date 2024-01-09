cecabc7ea86630e8b98eaa2294e2402ba963d0b0
TamichiRyuto <sp3h6ts9tamichi@hotmail.co.jp>
2024-01-09 14:33:33 +0900

    Busインタフェースとバス時刻表の仮データを追加

--- src/app-server/src/screens/BusScreen.tsx
+++ src/app-server/src/screens/BusScreen.tsx
@@ -2,17 +2,29 @@
 // シャトルバスの時刻表を表示するScreen
 
 import React from 'react';
-import { SafeAreaView, Text, StyleSheet } from 'react-native';
+import { FlatList, SafeAreaView, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import Bus from '../interface/Bus';
+import busData from '../test_data/bus_output.json'; // バス時刻表の仮データJSONファイル
+
+const renderBusItem = ({ item }: { item: Bus }) => (
+  <TouchableOpacity style={styles.busItem}>
+    <Text>
+      <Text style={styles.busNum}>{item.bus_num}  </Text>
+      <Text style={styles.busTimeNormal}>{item.start_time}</Text>
+    </Text>
+  </TouchableOpacity>
+);
 
 const BusScreen: React.FC = () => {
   // 現在時刻と最も近い発車時刻の便を強調表示したい
   return (
-    <SafeAreaView 
-      style={{padding: 20, flex: 1, alignItems: 'center', justifyContent: 'center'}}
-    >
-      <Text style={styles.largeText}>
-        ここにシャトルバスの時刻表を表示
-      </Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.largeText}>中百舌鳥⇔杉本 シャトルバス時刻表</Text>
+      <FlatList
+        data={busData}
+        keyExtractor={(item) => item.bus_num}
+        renderItem={renderBusItem}
+      />
     </SafeAreaView>
   );
 };
@@ -27,6 +39,28 @@ const styles = StyleSheet.create({
   },
   largeText: {
     fontSize: 20, // 大きい文字の大きさ
+  },
+  container: {
+    flex: 1,
+    padding: 30,
+    marginTop: 20,
+  },
+  busItem: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    paddingVertical: 8,
+  },
+  busNum: {
+    fontSize: 26,
+    color: '#84919e'
+  },
+  busTimeNormal: {
+    fontSize: 28,
+    color: '#005aff',
+  },
+  busTimeHighlight: {
+    fontSize: 20,
+    color: '#ff4b00'
   },
 });
 
