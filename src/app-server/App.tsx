7fe0caf88f25190044a2980c1f46dc79560b1f60
Yuichiro Iwashita <9864901+yiRMT@users.noreply.github.com>
2024-01-11 05:57:37 +0100

    StatusBarを追加
    - これでiPhone上でステータスバーが消失する問題が解決したはず

--- src/app-server/App.tsx
+++ src/app-server/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './src/navigation/AppNavigator';
 
 const App: React.FC = () => {
   return (
     <NavigationContainer>
+      <StatusBar/>
       <AppNavigator />
     </NavigationContainer>
   );
